6458d14a0d9272fff016379d537517c9825488b6
dostybayeva <dostybayeva@kolesa.kz>
2022-12-18 20:18:08 +0600

    тесты на функции nameIsValid, fullTrim, getTotal

--- specs/app.spec.js
+++ specs/app.spec.js
@@ -1,8 +1,71 @@
 import { nameIsValid, fullTrim, getTotal } from '../src/app.js'
 
-/**
- * Для проверки, что jest настроен правильно. Можно удалить
- */
-test('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3);
+describe('Testing function nameIsValid', () => {
+    test('nameIsValid is defined', () => {
+        expect(nameIsValid).toBeDefined();
+    });
+    test('type of nameIsvalid is function', () => {
+        expect(typeof nameIsValid).toBe('function');
+    });
+    test('name length < 2', () => {
+        expect(nameIsValid('A')).toBeFalsy();
+    });
+    test('name length = 2', () => {
+        expect(nameIsValid('Ai')).toBeTruthy();
+    });
+    test('name length > 2', () => {
+        expect(nameIsValid('Aiz')).toBeTruthy();
+    });
+    test('name includes white spaces', () => {
+        expect(nameIsValid('A i')).toBeFalsy();
+    });
+    test('name is number', () => {
+        expect(nameIsValid(123)).toBeFalsy();
+    });
 });
+
+describe('Testing function fullTrim', () => {
+    test('fullTrim is defined', () => {
+        expect(fullTrim).toBeDefined();
+    });
+    test('type of fullTrim is function', () => {
+        expect(typeof fullTrim).toBe('function');
+    });
+    test('fullTrim replace white spaces', () => {
+        expect(fullTrim('Строчка с пробелами')).toBe('Строчкаспробелами')
+    });
+})
+
+describe('Testing function getTotal', () => {
+    test('getTotal is defined', () => {
+        expect(getTotal).toBeDefined();
+    });
+    test('type of getTotal is function', () => {
+        expect(typeof getTotal).toBe('function');
+    });
+    test('getTotal with all arguments', () => {
+        expect(getTotal([{name: 'помидоры', price: 20, quantity: 10}], 5)).toBe(190);
+    });
+    test('getTotal without name', () => {
+        expect(getTotal([{price: 20, quantity: 10}], 10)).toBe(180);
+    });
+    test('getTotal without discount ', () => {
+        expect(getTotal([{price: 20, quantity: 10}])).toBe(200);
+    });
+    test('getTotal with only one item', () => {
+        expect(getTotal([{price: 20}])).toBeNaN();
+    });
+    test('discount = 0', () => {
+        expect(getTotal([{price: 20, quantity: 10}], 0)).toBe(200);
+    });
+    test('discount is not number type', () => {
+        expect(function() {
+            getTotal([{price: 20, quantity: 10}], '10')
+        }).toThrowError('Скидка должна быть числом');
+    });
+    test('discount = -1', () => {
+        expect(function() {
+            getTotal([{price: 20, quantity: 10}], -1)
+        }).toThrowError('Процент скидки не может быть отрицательным');
+    });
+})
