31416a83550022d395da720cb311d01774f3ab23
alina <alinkina-pochaa@mail.ru>
2022-12-27 16:39:44 +0300

    homework unit-tests

--- specs/app.spec.js
+++ specs/app.spec.js
@@ -1,8 +1,54 @@
-import { nameIsValid, fullTrim, getTotal } from '../src/app.js'
+import { nameIsValid, fullTrim, getTotal } from '../src/app.js';
 
-/**
- * Для проверки, что jest настроен правильно. Можно удалить
- */
-test('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3);
+describe('nameIsValid', function () {
+  test('true with four symbols', function () {
+    expect(nameIsValid('ffff')).toBeTruthy();
+  });
+  test('false with one symbol', function () {
+    expect(nameIsValid('f')).toBeFalsy();
+  });
+  test('false with space', function () {
+    expect(nameIsValid('f ff')).toBeFalsy();
+  });
+});
+
+describe('fullTrim', function () {
+  test('is function', () => {
+    expect(typeof fullTrim).toBe('function');
+  });
+  test('imports without error', function () {
+    expect(fullTrim).toBeTruthy();
+  });
+  test('check trim of space', function () {
+    expect(fullTrim('hh hh')).toBe('hhhh');
+  });
+});
+
+describe('getTotal parametric test', () => {
+  test('check error Nan', () => {
+    expect(() => getTotal(10, 'hh')).toThrowError('Скидка должна быть числом');
+  });
+  test('check error percent', () => {
+    expect(() => getTotal(10, -1)).toThrowError(
+      'Процент скидки не может быть отрицательным'
+    );
+  })
+
+  test.each`
+    price      | op     | quantity      | expected
+    ${100}     | ${'-'} | ${10}         | ${90}
+  `('$price $op $quantity = $expected', ({ price, op, quantity, expected }) => {
+    if (expected === 'error') {
+      expect(() => getTotal(price, op, quantity)).toThrow();
+    } else {
+      expect(getTotal(price, op, quantity)).toBe(expected);
+    }
+  });
+  // const items = [
+  //   { items: [], discount:1,expected: 0 },
+  //   // { items: [10,10], expected: 100 },
+  // ];
+  // test.each(items)(`getTotal($items)`, ([items, expected]) => {
+  //   expected(getTotal(items)).toBe(expected);
+  // });
 });
