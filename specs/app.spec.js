096d48295876427a0ee9cd00b07d9e15b98c985a
Anna Voskresenskova <a.voskresenskova@atomyze.ru>
2023-01-02 20:48:54 +0300

    API tests to homework

--- specs/app.spec.js
+++
@@ -1,8 +0,0 @@
-import { nameIsValid, fullTrim, getTotal } from '../src/app.js'
-
-/**
- * Для проверки, что jest настроен правильно. Можно удалить
- */
-test('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3);
-});
