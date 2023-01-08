9e71f52ab98be2eacab8c270867c9339fa301043
olemoreflipflop <fil.olga.ev@gmail.com>
2023-01-08 23:07:16 +0300

    1st part of HW without .env

---
+++ specs/framework/config.js
@@ -0,0 +1,9 @@
+const config = {
+    baseUrl: 'https://bookstore.demoqa.com',
+    credentials: {
+        userName: 'demoUser1',
+        password: 'Demo1234567890!'
+    }
+}
+
+export default config;
