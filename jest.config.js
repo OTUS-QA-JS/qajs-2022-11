f040cfd454fe33ae979d6693ac7e6f11c7598191
Dmitriy Solodov <dsolodov@avito.ru>
2023-02-17 09:44:03 +0400

    add jest-html reporters

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,14 @@ export default {
     testMatch: ['**/specs/**/*.spec.*'],
     "transform": {
         "^.+\\.[t|j]sx?$": "babel-jest"
-    }
+    },
+    reporters: [
+        "default",
+        [
+            "jest-html-reporters", {
+                "publicPath": "./jest-html-report",
+                "filename": "report.html"
+            }
+        ]
+    ]
 }
