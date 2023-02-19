fa8064672b756fd50430d3fece857e219e8c47a8
olemoreflipflop <fil.olga.ev@gmail.com>
2023-02-19 15:57:06 +0300

    check actions

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,13 @@ export default {
     "transform": {
         "^.+\\.[t|j]sx?$": "babel-jest"
     },
-    reporters: ['default', 'jest-allure'],
+    reporters: ['default', 'jest-allure', [
+        "jest-html-reporters", {
+            "publicPath": "./jest-html-report",
+            "filename": "report.html",
+            "openReport": true
+        }
+    ]],
     testRunner: 'jest-jasmine2',
     setupFilesAfterEnv: ['jest-allure/dist/setup']
 }
