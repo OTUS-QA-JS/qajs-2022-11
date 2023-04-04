d7b120a09999e85732bdf66c774a90b75d35114f
strizhkova <strizhkova@banki.ru>
2023-04-04 23:45:44 +0300

    Доделала задание

---
+++ specs/boards/api-test.spec.js
@@ -0,0 +1,48 @@
+import config from "../user/config.js";
+
+import supertest from "supertest";
+
+let trello = supertest(config.url);
+
+describe('GET boards', () => {
+    test('Метод должен существовать', async () => {
+        const res = await supertest(config.url)
+            .get('/1/members/me/boards')
+            .send(config.creds);
+
+        expect(res.statusCode).not.toEqual(404);
+    }),
+
+    test('Cтатус с кодом ошибки если токен неверный', async () => {
+        const res = await supertest(config.url)
+            .get('/1/members/me/boards')
+            .set('Accept', 'application/json')
+            .send({key: '25c3743b55c428cbc7684ebb469d9c12', token: 'demo3'})
+
+        expect(res.status).toEqual(401);
+      }),
+
+    test('Проверка досок у юзера', async () => {
+        const res = await supertest(config.url)
+        .get('/1/members/me/boards')
+        .send(config.creds);
+
+        expect(res.body.length).not.toEqual(0);
+    }),
+
+    test('В объекте возвращается доска с названием FOR OTUS', async () => {
+        const res = await supertest(config.url)
+        .get('/1/members/me/boards')
+        .send(config.creds);
+
+        expect(res.body[0].name).toEqual('FOR OTUS');
+    })
+
+    test('В объекте не возвращаются несуществующие урлы', async() => {
+        const res = await supertest(config.url)
+        .get('/1/members/me/boards')
+        .send(config.creds);
+
+        expect(res.body[0].url).not.toEqual('https://trello.com/b/xZcfugabuga/for-otus');
+    })
+});
--- specs/user/config.js
+++ specs/user/config.js
@@ -1,7 +1,12 @@
 const config = {
-    url: 'https://trello.com',
-    credentials:{
-        email: 'blackchasmfel@gmail.com',
-        password: 'testowitch'
+    url: 'https://api.trello.com',
+    creds: {
+        key: '25c3743b55c428cbc7684ebb469d9c12',
+        token: 'ATTA6753b7428c211cce1f2eb2e4278b50ebe1aab9660d1e11d87d3c9a875ab5ddd1E1181884'
     }
 }
+
+export default config;
+
+/*process.env.API_USERNAME
+process.env.API_PASSWORD*/
