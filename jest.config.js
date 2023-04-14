05167d0a7935b8944a5945ea9656b853307f6839
Ekaterina <kateluchs@gmail.com>
2023-04-14 19:30:14 +0300

    update

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
-export default {
-    testMatch: ['**/specs/**/*.spec.*'],
-    "transform": {
-        "^.+\\.[t|j]sx?$": "babel-jest"
-    }
-}
+module.exports = {
+  testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.js'],
+  reporters: ['default'],
+  moduleFileExtensions: ['js', 'json'],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  testMatch: ['**/specs/*.spec.*'],
+  globals: {
+    testTimeout: 50000,
+  },
+  verbose: true,
+};
