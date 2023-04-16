066362ba89e48f659c4019bf3ab8d21bfa8e74d3
Bu <m.bukatsich@knomary.by>
2023-04-17 02:15:43 +0300

    added app.spec.js with homework

--- specs/app.spec.js
+++ specs/app.spec.js
@@ -1,8 +1,64 @@
 import { nameIsValid, fullTrim, getTotal } from '../src/app.js'
 
-/**
- * Для проверки, что jest настроен правильно. Можно удалить
- */
+/* 
 test('adds 1 + 2 to equal 3', () => {
     expect(1 + 2).toBe(3);
 });
+ */
+
+describe('function nameIsValid coverage', () => {
+    test('nameIsValid is true', () => {
+        expect(nameIsValid('???')).toBe(true)
+    });
+    test('nameIsValid is false if length = 1', () => {
+        expect(nameIsValid('?')).toBe(false)
+    });
+    test('nameIsValid is false if includes spaceButt', () => {
+        expect(nameIsValid(' ')).toBe(false)
+    });
+});
+
+describe('function fullTrim coverage', () => {
+    test('fullTrim delete space from string', () => {
+        expect(fullTrim(' ')).toBe('')
+    });
+    test('fullTrim delete two spaces from string with symbol', () => {
+        expect(fullTrim(' ? ')).toBe('?')
+    });
+    test('fullTrim throws an error when get a number', () => {
+        expect(() => fullTrim(1)).toThrow();
+    });
+})
+
+
+/* 
+describe('function getTotal parametric coverage', () => {
+    test.each`
+    price   | quantity      | result
+    ${10}   | ${10}         |   ${100}
+    ${10}   | ${1}          |   ${10}
+    ${10}   | ${0}          |   ${0}
+    ${0}    | ${10}          |   ${0}
+    ${10}   | ${10}         |   ${100}
+    ${-1}   | ${10}         |   ${10}
+    ${-1}   | ${-1}         |   ${1}
+    ${10}   | ${10}         |   ${'error'}
+    ${'?'}   | ${-1}        |   ${'error'}
+    ${100}   | ${'?'}       |   ${'error'}
+    `('$function $quantity $result', ({price, quantity, result}) => {
+        if(result === 'error') {
+            expect(() => getTotal(price, '*', quantity)).toThrow
+        } else {
+            expect('amount of order', () => {
+                expect(getTotal(price, '*', quantity)).toBe(result)
+            })
+        }
+    })
+    
+})
+// После написания теста выше, я понял что он вообще не работает
+// и не проверяет работу getTotal впринципе.
+// Вижу что items это массив с объектом и его параметрами, но как его
+// разложить по параметрам, а потом это вызвать я понятия не имею.
+// 
+ */
