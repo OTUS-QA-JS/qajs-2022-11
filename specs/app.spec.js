3115c7e17389a5efb31b3eccd5c1f7660fe5e3b7
nikitalitvinkov <megapyps123@yandex.ru>
2023-04-25 20:08:03 +0500

    dzHttpZaprosi

--- specs/app.spec.js
+++ specs/app.spec.js
@@ -1,8 +1,99 @@
-import { nameIsValid, fullTrim, getTotal } from '../src/app.js'
+/*import fetch from "node-fetch";
+import {discribe, expect, test} from '@jest/globals';*/
 
-/**
- * Для проверки, что jest настроен правильно. Можно удалить
- */
-test('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3);
-});
+// создание пользователя с ошибкой логин уже используется 
+
+test.skip ( 'add user', async() => {
+    const response = await fetch ('https://bookstore.demoqa.com/Account/v1/User', {
+        method: 'POST',
+        headers: { 'Content-Type' : 'application/json' },
+        body: JSON.stringify({
+      
+            userName: 'vasya',
+            password: 'Vasya323#',
+        })
+    
+    })
+    
+    const data = await response.json();
+console.log(data);
+console.log(data.status);    
+    });
+
+
+    // создание пользователя с ошибкой пароль не подходит 
+
+    test.skip( 'add user', async() => {
+        const response = await fetch ('https://bookstore.demoqa.com/Account/v1/User', {
+            method: 'POST',
+            headers: { 'Content-Type' : 'application/json' },
+            body: JSON.stringify({
+          
+                userName: 'vasya',
+                password: 'Vasya323',
+            })
+        
+        })
+        
+        const data = await response.json();
+    console.log(data);
+    console.log(data.status);    
+        });
+
+        // создание пользователя успешно 
+
+        test.skip( 'add user', async() => {
+            const response = await fetch ('https://bookstore.demoqa.com/Account/v1/User', {
+                method: 'POST',
+                headers: { 'Content-Type' : 'application/json' },
+                body: JSON.stringify({
+              
+                    userName: 'vasya1',
+                    password: 'Vasya323#',
+                })
+            
+            })
+            
+            const data = await response.json();
+        console.log(data);
+        console.log(data.status);    
+            });
+
+            // генерация токена с ошибкой 
+            test ( 'generation token error ', async() => {
+                const response = await fetch ('https://bookstore.demoqa.com/Account/v1/GenerateToken', {
+                    method: 'POST',
+                    headers: { 'Content-Type' : 'application/json' },
+                    body: JSON.stringify({
+                  
+                        userName: 'vasya1',
+                        password: 'Vasya323',
+                    })
+                
+                })
+                
+                const data = await response.json();
+            console.log(data);
+            console.log(data.status);    
+                });
+
+                // генерация токена успешно 
+
+                test ( 'generation token error ', async() => {
+                    const response = await fetch ('https://bookstore.demoqa.com/Account/v1/GenerateToken ', {
+                        method: 'POST',
+                    
+                        headers: { 'Content-Type' : 'application/json' },
+                        body: JSON.stringify({
+                      
+                            userName: 'vasya1',
+                            password: 'Vasya323#',
+                        })
+                    
+                    })
+                    
+                    const data = await response.json();
+                console.log(data);
+                console.log(data.status);    
+                    });
+    
