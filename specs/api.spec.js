89a30aae7f43b23fff8ce4568933f9c7c6f99408
artyom1107@mail.ru <Slavahitleru1488>
2023-05-09 00:30:03 +0500

    added task 2 - public api tests

--- specs/api.spec.js
+++ specs/api.spec.js
@@ -43,14 +43,14 @@ test('Create user - Succeeded', async () => {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            userName: 'test11',
+            userName: 'test12',
             password: 'Testuser1!'
         })
     })
     const data = await response.json();
     expect(data).toHaveProperty('books');
     expect(response.status).toEqual(201);
-    expect(data.username).toEqual('test11');
+    expect(data.username).toEqual('test12');
 })
 
 test('Generate a token - Succeeded', async () => {
@@ -87,4 +87,79 @@ test('Generate a token - With error', async () => {
     expect(response.status).toEqual(400);
     expect(data.code).toEqual('1200');
     expect(data.message).toEqual('UserName and Password required.');
+})
+
+//task 2 - public api tests (https://swapi.dev/ and https://restful-api.dev/ )
+
+test('List of all SW characters', async () => {
+    const response  = await fetch('https://swapi.dev/api/people',{
+        method: 'GET',
+   })
+    const data = await response.json();
+    expect(response.status).toEqual(200);
+    expect(data.count).toEqual(82);
+    expect(Array.isArray(data.results)).toBe(true); //проверяю что в results получу массив 
+})
+
+test('Add new object - No errors', async () => {
+    const response  = await fetch('https://api.restful-api.dev/objects',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: 'test',
+        })
+   })
+    const data = await response.json();
+    expect(response.status).toEqual(200);
+    expect(response.statusText).toEqual('OK');
+    expect(data.name).toEqual('test');
+})
+
+test('Add new object - Post empty body', async () => {
+    const response  = await fetch('https://api.restful-api.dev/objects',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            
+        })
+   })
+    const data = await response.json();
+    expect(response.status).toEqual(200);
+    expect(response.statusText).toEqual('OK');
+    expect(data.name).toEqual(null);
+})
+
+test('Check data in response object', async () => {
+    const response  = await fetch('https://api.restful-api.dev/objects/7',{
+        method: 'GET',
+        
+   })
+    const data = await response.json();
+    expect(response.status).toEqual(200);
+    expect(response.statusText).toEqual('OK');
+    expect(data.data.year).toEqual(2019);
+    expect(data.data['Hard disk size']).toEqual('1 TB')
+})
+
+test('Correct update', async () => {
+    const response  = await fetch('https://api.restful-api.dev/objects/ff80818187f8fe360187fd3ec73600d9',{
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: 'Super car',
+            data:{'color':'black'}
+        })
+        
+   })
+    const data = await response.json();
+    expect(response.status).toEqual(200);
+    expect(response.statusText).toEqual('OK');
+    expect(data).toHaveProperty('updatedAt');
+    expect(data.name).toEqual('Super car')
 })
