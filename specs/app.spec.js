8bce47e94957d2b1376f9c8394b79619bd84d683
Yaroslav Chaadaev <yaroslav.chaadaev@technopark.ru>
2023-07-19 16:58:48 +0300

    First commit

--- specs/app.spec.js
+++ specs/app.spec.js
@@ -1,10 +1,20 @@
-import { expect, test } from '@jest/globals'
+import { expect, it } from "@jest/globals";
+import fetch from "node-fetch";
 // eslint-disable-next-line no-unused-vars
-import { nameIsValid, fullTrim, getTotal } from '../src/app.js'
+import { nameIsValid, fullTrim, getTotal } from "../src/app.js";
 
-/**
- * Для проверки, что jest настроен правильно. Можно удалить
- */
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3)
-})
+describe("Test Example", () => {
+  it("adds 1 + 2 to equal 3", () => {
+    expect(1 + 2).toBe(3);
+  });
+});
+
+describe("dummyjson API Testing", () => {
+  it("Sample Text", async () => {
+    const URI = "https://dummyjson.com/products";
+    const response = await fetch(URI);
+    const data = await response.json();
+    console.log(data);
+    expect(response.status).toBe(200);
+  });
+});
