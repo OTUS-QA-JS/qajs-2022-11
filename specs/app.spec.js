6b4f1348d2dac65961ca2c58c7a039add7325608
Anastasiya Dolmatova <nastydolmatovaa@gmail.com>
2023-07-23 16:38:08 +0300

    unit tests

--- specs/app.spec.js
+++ specs/app.spec.js
@@ -2,9 +2,61 @@ import { expect, test } from '@jest/globals'
 // eslint-disable-next-line no-unused-vars
 import { nameIsValid, fullTrim, getTotal } from '../src/app.js'
 
-/**
- * Для проверки, что jest настроен правильно. Можно удалить
- */
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3)
+// nameIsValid
+
+describe('Check if name is valid', () => {
+  test.each([
+  ['Nastya', true],
+  ['Nastya Ivanova', false],
+  ['Li', true],
+  ['L', false],
+  ])('expects value %p returns %p', (name, expected) => {
+    expect(nameIsValid(name)).toBe(expected);
+  });
+});
+
+// fullTrim
+
+describe('string without spaces', () => {
+  test.each([
+    ['Hello , World !', 'Hello,World!'],
+    [' Good bye ', 'Goodbye'],
+    ['   ', ''],
+    ['', ''],
+  ])('expects value %p returns %p', (text, expected) => {
+    expect(fullTrim(text)).toBe(expected);
+  });
+});
+
+// getTotal
+
+describe('getTotal function', () => {
+  it('import without error', function () {
+    expect(getTotal).toBeTruthy();
+    expect(typeof getTotal).toBe('function');
+  })
+  it('Check calculation without discount', () => {
+    const items = [
+      { price: 10, quantity: 1 },
+      { price: 10, quantity: 9 },
+    ];
+    const result = getTotal(items);
+    expect(result).toBe(100);
+  }) 
+  it('Check calculation with positive discount', () => {
+    const items = [
+      { price: 10, quantity: 10 },
+      { price: 10, quantity: 5 },
+    ];
+    const discount = 10;
+    expect(getTotal(items, discount)).toBe(135);
+  })
+  it('Incorrect discount type throws an error', () => {
+    const testFunc = () => getTotal([], '10%');
+    expect(testFunc).toThrowError('Скидка должна быть числом');
+  })
+  it('Negative discount throws an error', () => {
+    const testFunc = () => getTotal([], -10);
+    expect(testFunc).toThrowError('Процент скидки не может быть отрицательным');
+  })
 })
