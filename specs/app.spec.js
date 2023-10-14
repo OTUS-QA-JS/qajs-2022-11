03d52d692526d0c211cc1b72daebc392797a0f17
elfedyukova <elfedyukova@yandex.ru>
2023-10-14 14:33:54 +0300

    add homework

--- specs/app.spec.js
+++ specs/app.spec.js
@@ -1,9 +1,82 @@
-// eslint-disable-next-line no-unused-vars
 import { nameIsValid, fullTrim, getTotal } from "../src/app.js";
+describe("getTotal 100% coverage", () => {
+  describe("getTotal check imports", () => {
+    test("getTotal check import", () => {
+      expect(typeof getTotal).toBe("function");
+      expect(getTotal).toBeDefined();
+    });
+  });
+  describe("getTotal check throw error", () => {
+    test("check discount not a number", () => {
+      expect(() => getTotal([{ price: 10, quantity: 10 }], "5")).toThrow(
+        "Скидка должна быть числом",
+      );
+    });
+    test("check discount < 0", () => {
+      expect(() => getTotal([{ price: 10, quantity: 10 }], -6)).toThrow(
+        "Процент скидки не может быть отрицательным",
+      );
+    });
+  });
 
-/**
- * Для проверки, что jest настроен правильно. Можно удалить
- */
-test("adds 1 + 2 to equal 3", () => {
-  expect(1 + 2).toBe(3);
+  describe("getTotal check function", () => {
+    test("check discount 10", () => {
+      expect(getTotal([{ price: 10, quantity: 10 }], 10)).toBe(90);
+    });
+    test("check discount 100", () => {
+      expect(getTotal([{ price: 10, quantity: 10 }], 100)).toBe(0);
+    });
+  });
+});
+describe("nameIsValid 100% coverage", () => {
+  describe("nameIsValid check imports", () => {
+    test("check import", () => {
+      expect(nameIsValid).toBeDefined();
+      expect(typeof nameIsValid).toBe("function");
+    });
+  });
+  describe("nameIsValid check false", () => {
+    test("check nameIsValid false 1", () => {
+      expect(nameIsValid("t")).toBe(false);
+    });
+    test("check nameIsValid false number", () => {
+      expect(nameIsValid("t")).toBe(false);
+    });
+  });
+
+  describe("nameIsValid check true", () => {
+    test("check nameIsValid true", () => {
+      expect(nameIsValid("Lena")).toBe(true);
+    });
+    test("check nameIsValid true 2 characters", () => {
+      expect(nameIsValid("Ll")).toBe(true);
+    });
+  });
+});
+describe("fullTrim 100% coverage", () => {
+  describe("fullTrim check imports", () => {
+    test("check import", () => {
+      expect(fullTrim).toBeDefined();
+      expect(typeof fullTrim).toBe("function");
+    });
+  });
+
+  describe("fullTrim check function", () => {
+    test("check number", () => {
+      expect(fullTrim).toBeDefined();
+    });
+    test("check discount", () => {
+      expect(fullTrim).toBeDefined();
+    });
+  });
+
+  describe("fullTrim parametric test", () => {
+    test.each`
+      a          | expected
+      ${"test "} | ${"test"}
+      ${" test"} | ${"test"}
+    `("$a = $expected", ({ a, expected }) => {
+      expect(fullTrim(a)).toBe(expected);
+    });
+  });
 });
