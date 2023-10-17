ac9cb99bd4bc8a3ed47beb8272cc35ba32f095f6
Aleksei <alexei.misuna@yandex.ru>
2023-10-17 10:06:19 +0700

    added new tests

--- specs/app.spec.js
+++ specs/app.spec.js
@@ -1,9 +1,56 @@
 // eslint-disable-next-line no-unused-vars
 import { nameIsValid, fullTrim, getTotal } from "../src/app.js";
 
-/**
- * Для проверки, что jest настроен правильно. Можно удалить
- */
-test("adds 1 + 2 to equal 3", () => {
-  expect(1 + 2).toBe(3);
+describe('nameIsValid function', () => {
+  it('import without error', () => {
+    expect(nameIsValid).toBeDefined();
+  });
+  it('Name have three letters', () => {
+    const nameLenght = nameIsValid('Kim')
+    expect(nameLenght).toBeTruthy();
+  });
+  it('Name have three letters', () => {
+    const nameLenght = nameIsValid('K')
+    expect(nameLenght).toBeFalsy();
+  });
+  it('Name have three letters', () => {
+    const nameLenght = nameIsValid('Ki')
+    expect(nameLenght).toBeTruthy();
+  });
 });
+
+describe('clear string', () => {
+  it('import without error', () => {
+    expect(fullTrim).toBeDefined();
+  });
+  it('Space before text', () => {
+    const text = fullTrim(' name');
+    expect(text).toBeTruthy();
+  });
+  it('Space after text', () => {
+    const text = fullTrim('na me');
+    expect(text).toBeTruthy();
+  });
+  it('Text not string', () => {
+    const text = fullTrim('');
+    expect(text).toBeFalsy();
+  });
+});
+
+describe('Calculating order amount', () => {
+  test.each([
+    { items: [{ price: 10, quantity: 10 }], discount: 0, expected: 100 },
+    { items: [{ price: 10, quantity: 1 }, { price: 10, quantity: 9 }], discount: 0, expected: 100 },
+    { items: [{ price: 10, quantity: 10 }], discount: 10, expected: 90 }
+  ])('correctСalculation', ({ items, discount, expected }) => {
+    const total = getTotal(items, discount)
+    expect(total).toBe(expected)
+  })
+})
+test.each([
+  { items: [{ price: 10, quantity: 10 }], discount: 'test', expected: 'Скидка должна быть числом' },
+  { items: [{ price: 10, quantity: 10 }], discount: -10, expected: 'Процент скидки не может быть отрицательным' }
+])('negativeCase', ({ items, discount, expected }) => {
+const total = () => getTotal(items, discount)
+expect(total).toThrow()
+})
