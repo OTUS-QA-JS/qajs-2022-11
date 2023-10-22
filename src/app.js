ab0dd7baa44c5813ca4c57b4ea52682fca2fb9a7
Aleksei <aleksei.misuna@yandex.ru>
2023-10-22 12:11:27 +0700

    Added getScore()

--- src/app.js
+++ src/app.js
@@ -40,3 +40,21 @@ export const getTotal = (items = [], discount = 0) => {
   }, 0);
   return total - (total * discount) / 100;
 };
+
+
+const getScore = () => {
+  const scores = {
+    Anna: 10,
+    Olga: 1,
+    Ivan: 5,
+    Aleksei: 7,
+    Max: 3,
+    Evgen: 9
+  };
+  let sumScores = 0;
+  for (let key in scores) {
+    sumScores = sumScores + scores[key];
+  };
+  console.log(sumScores)
+};
+getScore()
