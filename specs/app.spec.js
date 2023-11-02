d2ec4407d26fffb1430078fa0bb83ad5065c5443
andreypopov <andrey.popov@elementpay.io>
2023-11-02 23:19:44 +0700

    test commit

--- specs/app.spec.js
+++ specs/app.spec.js
@@ -4,6 +4,49 @@ import { nameIsValid, fullTrim, getTotal } from "../src/app.js";
 /**
  * Для проверки, что jest настроен правильно. Можно удалить
  */
-test("adds 1 + 2 to equal 3", () => {
-  expect(1 + 2).toBe(3);
-});
+
+describe('nameIsValid', function () {
+  it('imports without error', function () {
+    expect(nameIsValid).toBeTruthy()
+  })
+  it('is function', () => {
+    expect(typeof nameIsValid).toBe('function')
+  })
+  it('should return true if name is valid', () => {
+    expect(nameIsValid("Liza")).toBe(true)
+    expect(nameIsValid('asdasdasdadadsasd1233')).toBe(true)
+    expect(nameIsValid('Joe')).toBe(true)
+  });
+
+  it('should return false if name is invalid', () => {
+    expect(nameIsValid('')).toBe(false)
+    expect(nameIsValid('A')).toBe(false)
+    expect(nameIsValid('Test Name')).toBe(false)
+    expect(nameIsValid(undefined)).toBe(false)
+    expect(nameIsValid(null)).toBe(false)
+  });
+})
+
+
+describe('fullTrim', function () {
+  it('imports without error', function () {
+    expect(fullTrim).toBeTruthy()
+  })
+  it('is function', () => {
+    expect(typeof fullTrim).toBe('function')
+  })
+
+  it('should return empty string for null or undefined', () => {
+    expect(fullTrim(null)).toBe('')
+    expect(fullTrim(undefined)).toBe('')
+  });
+
+  it('should remove all whitespace characters from string', () => {
+    expect(fullTrim('  Hello   World  ')).toBe('HelloWorld')
+  })
+
+  it('should not change string without whitespaces', () => {
+    expect(fullTrim('Teststring')).toBe('Teststring')
+  });
+  
+  })
