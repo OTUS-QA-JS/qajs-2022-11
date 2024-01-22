5164c9a054d1a1dd4d6eb6b5c5c908843ac4b786
kuzminova <kuzminova.daria@mail.ru>
2024-01-22 23:26:52 +0300

    Add tests for app.js

--- specs/app.spec.js
+++ specs/app.spec.js
@@ -1,9 +1,109 @@
-// eslint-disable-next-line no-unused-vars
 import { nameIsValid, fullTrim, getTotal } from "../src/app.js";
 
-/**
- * Для проверки, что jest настроен правильно. Можно удалить
- */
-test("adds 1 + 2 to equal 3", () => {
-  expect(1 + 2).toBe(3);
+describe("Test nameIsValid function", () => {
+  test("Positive", () => {
+    expect(nameIsValid("Dasha")).toBe(true);
+  });
+
+  test("Negative", () => {
+    expect(nameIsValid("")).toBe(false);
+  });
+
+  const testCases = [
+    { name: "max", expected: true },
+    { name: "  \t", expected: false },
+    { name: null, expected: false },
+  ];
+
+  test.each(testCases)(
+    "Params Test with name '$name'",
+    ({ name, expected }) => {
+      expect(nameIsValid(name)).toBe(expected);
+    },
+  );
+});
+
+describe("Test fullTrim function", () => {
+  test("Positive", () => {
+    expect(fullTrim("   Dasha    ")).toBe("Dasha");
+  });
+
+  test("Negative", () => {
+    expect(fullTrim("")).toBe("");
+  });
+
+  const testCases = [
+    { str: "m  a   x", expected: "max" },
+    { str: "  \t", expected: "" },
+    { str: null, expected: "" },
+  ];
+
+  test.each(testCases)("Params Test with string 'str'", ({ str, expected }) => {
+    expect(fullTrim(str)).toBe(expected);
+  });
+});
+
+describe("Test getTotal function", () => {
+  test("Positive", () => {
+    expect(getTotal([{ price: 10, quantity: 10 }])).toBe(100);
+  });
+
+  test("Negative", () => {
+    expect(getTotal([])).toBe(0);
+  });
+
+  const posTestCases = [
+    {
+      num: 1,
+      items: [{ price: 10, quantity: 10 }],
+      discount: 0,
+      expected: 100,
+    },
+    {
+      num: 2,
+      items: [{ price: 10, quantity: 10 }],
+      discount: 300,
+      expected: -200,
+    },
+    {
+      num: 1,
+      items: [
+        { price: 10, quantity: 1 },
+        { price: 10, quantity: 9 },
+      ],
+      discount: 10,
+      expected: 90,
+    },
+  ];
+
+  test.each(posTestCases)(
+    "Positive Params Test $num",
+    ({ items, discount, expected }) => {
+      expect(getTotal(items, discount)).toBe(expected);
+    },
+  );
+
+  const negTestCases = [
+    {
+      num: 1,
+      items: [{ price: 10, quantity: 10 }],
+      discount: null,
+      expected: "Скидка должна быть числом",
+    },
+    {
+      num: 2,
+      items: [{ price: 10, quantity: 10 }],
+      discount: -100,
+      expected: "Процент скидки не может быть отрицательным",
+    },
+  ];
+
+  test.each(negTestCases)(
+    "Negative Params Test $num",
+    ({ items, discount, expected }) => {
+      expect(() => {
+        getTotal(items, discount);
+      }).toThrow(expected);
+    },
+  );
 });
